fbe0dc8b89bd8f378a24c58fb417a71e1ec6a1b0
laptop <rogerbm26@gmail.com>
2019-03-24 19:46:24 +0100

    html templates

--- carnisseria/urls.py
+++ carnisseria/urls.py
@@ -1,33 +1,15 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from django.utils import TIME_ZONE
-from django.views.generics import DetailView, ListView, UpdateView
-from carnisseria.models import Botiga, Carns
-from carnisseria.forms import BotigaForm, CarnsForn
-from carnisseria.views import BotigaCreate, CarnsCreate, BotigaDetail
+#from django.utils import TIME_ZONE
+#from django.views.generics import DetailView, ListView, UpdateView
+#from carnisseria.models import Botiga, Carns
+#from carnisseria.forms import BotigaForm, CarnsForn
+#from carnisseria.views import BotigaCreate, CarnsCreate, BotigaDetail
 
 urlpatterns = [
     # Restaurant details, ex.: /myrestaurants/restaurants/1/
-    url(r'^carnisseria/(?P<pk>\d+)/$',
-        BotigaDetail.as_view(),
-        name='botiga_detail'),
-
-    # Restaurant carns details, ex: /myrestaurants/restaurants/1/dishes/1/
-    url(r'^carnisseria/(?P<pkr>\d+)/carns/(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=Carns,
-            template_name='carnisseria/carns_detail.html'),
-            name='carns_detail'),
-
-    # Create a restaurant, /myrestaurants/restaurants/create/
-    url(r'^restaurants/create/$',
-        BotigaCreate.as_view(),
-        name='botiga_create'),
-
-    # Create a restaurant dish, ex.: /myrestaurants/restaurants/1/dishes/create/
-    url(r'^restaurants/(?P<pk>\d+)/carns/create/$',
-        CarnsCreate.as_view(),
-        name='carns_create'),
-
+    path('', views.base, name='homepage'),
+    path('carns/', views.carns, name='carns'),
+    path('precuinats/', views.precuinats, name='precuinats'),
 ]
